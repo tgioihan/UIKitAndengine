11bfcd52bb1b09605ada0a73d87986ddb79a82ca
unknown <tgioihan@gmail.com>
2014-07-29 17:59:17 +0700

    cm

---
+++ src/com/bestfunforever/andengine/uikit/entity/Sprite/SpriteImplement.java
@@ -0,0 +1,32 @@
+package com.bestfunforever.andengine.uikit.entity.Sprite;
+
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+public class SpriteImplement extends BaseSprite {
+
+	public SpriteImplement(float pX, float pY, float pWidth, float pHeight, ITextureRegion pTextureRegion,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pWidth, pHeight, pTextureRegion, pVertexBufferObjectManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onSelectedState() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onNormalState() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPressState() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
